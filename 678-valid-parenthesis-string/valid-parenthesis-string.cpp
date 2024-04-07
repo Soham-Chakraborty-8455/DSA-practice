e6612fab9a1afbde6337d4477858920fe9b07ae4
Soham Chakraborty <schakraborty8455@gmail.com>
2024-04-07 09:36:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (9.43%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public: 
+    bool checkValidString(string s) {
+        vector<vector<int>> memo(s.size(), vector<int>(s.size(), -1));
+        return isValidString(0, 0, s, memo);
+    }
+private: 
+    bool isValidString(int index, int openCount,
+        const string & str, vector < vector < int >> & memo) {
+        if (index == str.size()) {
+            return (openCount == 0);
+        }
+
+        if (memo[index][openCount] != -1) {
+            return memo[index][openCount];
+        }
+
+        bool isValid = false;
+        if (str[index] == '*') {
+            isValid |= isValidString(index + 1, openCount + 1, str, memo); // Treat '*' as '('
+            if (openCount) {
+                isValid |= isValidString(index + 1, openCount - 1, str, memo); // Treat '*' as ')'
+            }
+            isValid |= isValidString(index + 1, openCount, str, memo); // Treat '*' as empty
+        } else {
+            if (str[index] == '(') {
+                isValid = isValidString(index + 1, openCount + 1, str, memo); // Increment count for '('
+            } else if (openCount) {
+                isValid = isValidString(index + 1, openCount - 1, str, memo); // Decrement count for ')'
+            }
+        }
+        return memo[index][openCount] = isValid;
+    }
+};
