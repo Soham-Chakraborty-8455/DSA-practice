540b7e427a920a7c550731eb7b24dd257bd0d331
Soham Chakraborty <schakraborty8455@gmail.com>
2024-08-06 12:01:14 +0530

    Time: 3 ms (96.26%) | Memory: 22.2 MB (5.52%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        set<int> st;
+        for(auto it: nums){
+            st.insert(it);
+        }
+        int ind=0;
+        for(auto& it: st){
+            nums[ind]= it;
+            ind++;
+        }
+        return st.size();
+    }
+};
