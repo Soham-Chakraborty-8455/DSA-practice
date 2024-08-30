aab8607e7b48a2f585194e9c62a2c46b0a3b271a
Soham Chakraborty <schakraborty8455@gmail.com>
2024-08-30 18:19:07 +0530

    Time: 12 ms (49.86%) | Memory: 25.3 MB (17.96%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n= height.size();
-        vector<int> leftmax (n, 0); 
+        // vector<int> leftmax (n, 0); 
         vector<int> rightmax (n, 0);
-        leftmax[0]= height[0];
+        int leftmax= height[0];
         rightmax[n-1]= height[n-1];
-        for(int i=1; i<height.size(); i++){
-            leftmax[i]= max(leftmax[i-1], height[i]);
-        }
+        // for(int i=1; i<height.size(); i++){
+        //     leftmax[i]= max(leftmax[i-1], height[i]);
+        // }
         for(int i=n-2; i>=0; i--){
             rightmax[i]= max(rightmax[i+1], height[i]);
         }
         int sum=0;
         for(int i=0; i<height.size(); i++){
             // cout<<leftmax[i]<<" "<<rightmax[i]<< endl;
-            sum+= min(leftmax[i], rightmax[i]) - height[i];
+            leftmax= max(leftmax, height[i]);
+            sum+= min(leftmax, rightmax[i]) - height[i];
         }
         return sum;
     }
