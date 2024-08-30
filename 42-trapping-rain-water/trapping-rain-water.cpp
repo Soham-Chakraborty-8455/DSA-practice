5f834c0251a29402194023c9a8f02daa2e054127
Soham Chakraborty <schakraborty8455@gmail.com>
2024-08-30 18:17:17 +0530

    Time: 18 ms (13.01%) | Memory: 25.9 MB (13.19%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
-    int trap(vector<int>& h) {
-        int l=0,r=h.size()-1,lmax=INT_MIN,rmax=INT_MIN,ans=0;
-        while(l<r){
-            lmax=max(lmax,h[l]);
-            rmax=max(rmax,h[r]);
-            ans+=(lmax<rmax)?lmax-h[l++]:rmax-h[r--];
+    int trap(vector<int>& height) {
+        int n= height.size();
+        vector<int> leftmax (n, 0); 
+        vector<int> rightmax (n, 0);
+        leftmax[0]= height[0];
+        rightmax[n-1]= height[n-1];
+        for(int i=1; i<height.size(); i++){
+            leftmax[i]= max(leftmax[i-1], height[i]);
         }
-        return ans;
+        for(int i=n-2; i>=0; i--){
+            rightmax[i]= max(rightmax[i+1], height[i]);
+        }
+        int sum=0;
+        for(int i=0; i<height.size(); i++){
+            // cout<<leftmax[i]<<" "<<rightmax[i]<< endl;
+            sum+= min(leftmax[i], rightmax[i]) - height[i];
+        }
+        return sum;
     }
 };
+// [4 4 4 4 4 5]
+// [5 5 5 5 5 5]
