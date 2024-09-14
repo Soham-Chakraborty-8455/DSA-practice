cd0c308cba9e607611e881236eb23dd4fe841510
Soham Chakraborty <schakraborty8455@gmail.com>
2024-09-15 01:27:20 +0530

    Time: 6 ms (25.89%) | Memory: 17.5 MB (69.00%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -1,37 +1,14 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 private:
-    bool check(TreeNode* p, TreeNode* q){
-        if(p && !q) return false;
-        if(q && !p) return false;
-        if(!p && !q) return true;
-        bool left= check(p->left, q->left);
-        bool right= check(p->right , q->right);
-        return (p->val==q->val) && left && right;
+    bool check(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) return true; 
+        if (p == nullptr || q == nullptr) return false;
+        return (p->val == q->val) && check(p->left, q->right) && check(p->right, q->left);
     }
-    void f(TreeNode* root){
-        if(root==NULL) return;
-        TreeNode* l= root->left;
-        TreeNode* r= root->right;
-        root->left= r;
-        root->right= l;
-        f(root->left);
-        f(root->right);
-    }
+    
 public:
     bool isSymmetric(TreeNode* root) {
-        if(root==NULL) return false;
-        f(root->left);
+        if (root == nullptr) return true; 
         return check(root->left, root->right);
     }
 };
